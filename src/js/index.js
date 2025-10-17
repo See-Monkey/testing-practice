38feef1efd24ceee23f723ccaccd689e504bd8f8
See-Monkey <226230446+See-Monkey@users.noreply.github.com>
2025-10-16 19:48:28 -0600

    Setup jest and add capitalize function

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,13 @@
 console.log("Hi, Mom!");
 
 import "../css/style.css";
+
+export function capitalize(string) {
+  if (typeof string !== "string" || string.length === 0) {
+    return "";
+  }
+  if (!/^[A-Za-z]/.test(string)) {
+    return string;
+  }
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
--- src/js/index.test.js
+++ src/js/index.test.js
@@ -1,3 +1,13 @@
-test("test", () => {
-  expect(1).toBe(1);
+import { capitalize } from "./index.js";
+
+test("capitalize1", () => {
+  expect(capitalize("lower")).toBe("Lower");
+});
+
+test("capitalize2", () => {
+  expect(capitalize("2fast")).toBe("2fast");
+});
+
+test("capitalize3", () => {
+  expect(capitalize(2)).toBe("");
 });
